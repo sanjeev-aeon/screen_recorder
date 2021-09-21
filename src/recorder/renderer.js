630ccd8963f0c41e0546ec0281993211d23efa84
Sanjeev <sanjeev.shrivastava@siemens.com>
2021-09-21 23:58:19 +0530

    init

--- src/recorder/renderer.js
+++ src/recorder/renderer.js
@@ -1,13 +1,31 @@
 const video = document.querySelector('video');
 const stopRecButton = document.querySelector('.button-stop');
 const save = document.querySelector('.button-save');
-const selectScreenButton = document.querySelector('.button-select-screen');
-selectScreenButton.onclick = window.rContext.showContextMenu;
 
+const selectScreenButton = document.querySelector('.button-select-screen');
+
+let mediaRecorder = null;
+let chunks = [];
+selectScreenButton.onclick = () => {
+    window.rContext.showContextMenu();
+    mediaRecorder = null;
+    chunks = [];
+}
 
 function handleStream(stream) {
     video.srcObject = stream
     video.onloadedmetadata = () => video.play()
+
+    const options = { MimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+    mediaRecorder.start();
+    mediaRecorder.ondataavailable = function (e) {
+        chunks.push(e.data);
+    }
+    mediaRecorder.onstop = (e) => {
+        console.log(e);
+        video.pause();
+    }
 }
 
 const recordVideo = async (source) => {
@@ -28,6 +46,13 @@ const recordVideo = async (source) => {
         handleStream(stream)
     } catch (e) {
         console.error(e);
+    }
+};
+
+
+save.onclick = () => {
+    if (mediaRecorder) {
+        mediaRecorder.stop();
     }
 };
 
